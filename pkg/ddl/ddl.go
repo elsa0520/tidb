f0e408dd5b9ac3bc48731b27e06a4fc1d721661d
elsa0520 <elsa0520@163.com>
2026-09-15 19:15:46 +0800

    remove ddl innner forbidden function

--- pkg/ddl/ddl.go
+++ pkg/ddl/ddl.go
@@ -32,7 +32,6 @@ import (
 	"github.com/pingcap/errors"
 	"github.com/pingcap/failpoint"
 	"github.com/pingcap/tidb/pkg/config"
-	"github.com/pingcap/tidb/pkg/config/diagnosticmode"
 	"github.com/pingcap/tidb/pkg/config/kerneltype"
 	"github.com/pingcap/tidb/pkg/ddl/ingest"
 	"github.com/pingcap/tidb/pkg/ddl/logutil"
@@ -872,13 +871,6 @@ func (d *ddl) newDeleteRangeManager(mock bool) delRangeManager {
 
 // Start implements DDL.Start interface.
 func (d *ddl) Start(startMode StartMode, ctxPool *pools.ResourcePool) error {
-	if diagnosticmode.Enabled() {
-		// Schema loading is owned by Domain and does not need DDL execution resources.
-		if startMode != Normal {
-			return diagnosticmode.ErrDDLNotAllowed
-		}
-		return nil
-	}
 	if kerneltype.IsClassic() {
 		d.detectAndUpdateJobVersion()
 	}
@@ -1117,9 +1109,6 @@ func (d *ddl) CleanUpTempDirLoop(ctx context.Context, path string) {
 // Since ownerManager.CampaignOwner will start a new goroutine to run ownerManager.campaignLoop,
 // we should make sure that before invoking EnableDDL(), ddl is DISABLE.
 func (d *ddl) EnableDDL() error {
-	if diagnosticmode.Enabled() {
-		return diagnosticmode.ErrDDLNotAllowed
-	}
 	err := d.ownerManager.CampaignOwner()
 	return errors.Trace(err)
 }
@@ -1127,9 +1116,6 @@ func (d *ddl) EnableDDL() error {
 // DisableDDL disable this node to execute ddl.
 // We should make sure that before invoking DisableDDL(), ddl is ENABLE.
 func (d *ddl) DisableDDL() error {
-	if diagnosticmode.Enabled() {
-		return nil
-	}
 	if d.ownerManager.IsOwner() {
 		// If there is only one node, we should NOT disable ddl.
 		serverInfo, err := infosync.GetAllServerInfo(d.ctx)
@@ -1262,9 +1248,6 @@ func (d *ddl) cleanDeadTableLock(unlockTables []model.TableLockTpInfo, se model.
 
 // SwitchMDL enables MDL or disable MDL.
 func (d *ddl) SwitchMDL(enable bool) error {
-	if diagnosticmode.Enabled() {
-		return diagnosticmode.ErrDDLNotAllowed
-	}
 	isEnableBefore := vardef.IsMDLEnabled()
 	if isEnableBefore == enable {
 		return nil
